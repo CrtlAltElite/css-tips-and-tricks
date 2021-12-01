68de7957d8c7b3fb50f3599e1301551514bd7e31
Kevin Beier <kevinb@codingtemple.com>
2021-12-01 16:47:54 -0500

    intro screen

--- js/batman.js
+++ js/batman.js
@@ -29,4 +29,52 @@ const pauser=(audio,endTime)=>{
 return id;
 }
 
-document.addEventListener('click',()=>playlist(),{once:true})
+const startAnimation=()=>{
+    console.log('clicked')
+document.getElementsByTagName('body')[0].innerHTML=`
+    <div class="hero-image start-hidden">
+      <div class="hero-text start-hidden">
+        <h1>Bruce Wayne</h1>
+        <p class="type-writer">I am vengeance, I am the night, I am Batman!</p>
+        <img id="bat1" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat2" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat3" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat4" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat5" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat6" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat7" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat8" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat9" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat10" class="bat start-hidden" src="images/flyingbat.gif">
+
+        <img id="bat11" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat12" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat13" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat14" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat15" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat16" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat17" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat18" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat19" class="bat start-hidden" src="images/flyingbat.gif">
+        <img id="bat20" class="bat start-hidden" src="images/flyingbat.gif">
+      </div>
+    </div>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+`;
+    playlist();
+}
+
+document.getElementById('start-btn').addEventListener('click',()=>{startAnimation(); console.log('clicking')},{once:true})
+
+newDiv=document.createElement('div');
+newDiv.innerText="Enter the Batcave";
+newDiv.style.color='#FDE311';
+newDiv.style.position="absolute";
+newDiv.style.top="25%";
+newDiv.style.left="55%";
+newDiv.style.backgroundColor='black';
+newDiv.style.fontFamily='batman';
+newDiv.style.fontSize='2.5em';
+
+document.getElementsByClassName('slowmo-img')[0].onmouseover(setTimeout(()=>document.getElementsByClassName('slowmo-img')[0].appendChild(newDiv),3000))
